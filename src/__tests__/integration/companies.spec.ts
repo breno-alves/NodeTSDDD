9257784431832671a3e2a3b9d2e1f0f5a4ddf0fa
Breno Alves <breno550@hotmail.com>
2021-06-27 14:58:26 -0300

    integration tests for companies load

--- src/__tests__/integration/companies.spec.ts
+++ src/__tests__/integration/companies.spec.ts
@@ -2,6 +2,8 @@ import connection from '@shared/utils/connection';
 import request from 'supertest';
 import app from '@shared/infra/http/server';
 import CompaniesRepository from '@modules/companies/infra/typeorm/repositories/CompaniesRepository';
+import fs from 'fs';
+import path from 'path';
 
 describe('Companies', () => {
   beforeAll(async () => {
@@ -13,13 +15,94 @@ describe('Companies', () => {
     await connection.close();
   });
 
-  it('Should read CSV and extract data to DB', async () => {
-    await request(app).post(`/company/load`);
+  describe('Load', () => {
+    beforeEach(async () => {
+      await connection.clear();
+    });
 
-    const repo = new CompaniesRepository();
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [_, count] = await repo.findAllAndCount();
+    it('Should read CSV and extract data to DB', async () => {
+      await request(app).post(`/company/load`);
 
-    expect(count).toBeGreaterThan(0);
+      const repo = new CompaniesRepository();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [_, count] = await repo.findAllAndCount();
+
+      expect(count).toBeGreaterThan(0);
+    });
+
+    it('Should have no duplicate', async () => {
+      await request(app).post(`/company/load`);
+
+      const repo = new CompaniesRepository();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [companies, count] = await repo.findAllAndCount();
+
+      await request(app).post(`/company/load`);
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [companiesNew, countNew] = await repo.findAllAndCount();
+
+      expect(count).toBeGreaterThan(0);
+      expect(countNew).toEqual(count);
+    });
+
+    it('Should have only uppercased names', async () => {
+      await request(app).post(`/company/load`);
+
+      const repo = new CompaniesRepository();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [companies, count] = await repo.findAllAndCount();
+
+      const namesLowercase = companies.reduce((acc, { name }) => {
+        if (name !== name.toUpperCase()) {
+          acc.push(name);
+        }
+        return acc;
+      }, []);
+
+      expect(namesLowercase.length).toEqual(0);
+    });
+
+    it('Should have only lowercased website', async () => {
+      await request(app).post(`/company/load`);
+
+      const repo = new CompaniesRepository();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [companies, count] = await repo.findAllAndCount();
+
+      const websiteUppercase = companies.reduce((acc, { website }) => {
+        if (website != null && website !== website.toLowerCase()) {
+          acc.push(website);
+        }
+        return acc;
+      }, []);
+
+      expect(websiteUppercase.length).toEqual(0);
+    });
+  });
+
+  describe('Integrate', () => {
+    it.skip('Should integrate database data with integration data', async () => {
+      await request(app)
+        .post(`/company/load`)
+        .set('Content-type', 'multipart/form-data')
+        .attach(
+          'csv',
+          path.resolve(__dirname, '..', 'seeds', 'q2_clientData.csv'),
+        );
+
+      const repo = new CompaniesRepository();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const [companies, count] = await repo.findAllAndCount();
+
+      const websites = companies.reduce((acc, { website }) => {
+        if (website !== null) {
+          acc.push(website);
+        }
+        return acc;
+      }, []);
+
+      expect(websites.length).toBeGreaterThan(0);
+    });
   });
 });
